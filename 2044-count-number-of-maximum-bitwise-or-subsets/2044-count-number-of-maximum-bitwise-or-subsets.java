aaaebcbb7aa7bf88820eb9634a13a4f3957ae704
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-10-18 11:34:08 +0530

    Time: 9 ms (67.71%), Space: 40.5 MB (98.25%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOrValue = 0;
+        for (int num : nums) {
+            maxOrValue |= num;
+        }
+        return countSubsets(nums, 0, 0, maxOrValue);
+    }
+
+    private int countSubsets(
+        int[] nums,
+        int index,
+        int currentOr,
+        int targetOr
+    ) {
+        // Base case: reached the end of the array
+        if (index == nums.length) {
+            return (currentOr == targetOr) ? 1 : 0;
+        }
+
+        // Don't include the current number
+        int countWithout = countSubsets(nums, index + 1, currentOr, targetOr);
+
+        // Include the current number
+        int countWith = countSubsets(
+            nums,
+            index + 1,
+            currentOr | nums[index],
+            targetOr
+        );
+
+        // Return the sum of both cases
+        return countWithout + countWith;
+    }
+}
