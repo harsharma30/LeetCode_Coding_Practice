9cd13407e276415108680fa17401f4d6e9bb30ea
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2025-03-01 17:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (97.15%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,30 +1,26 @@
 class Solution {
-
     public int[] applyOperations(int[] nums) {
-        int n = nums.length;
-        int[] modifiedNums = new int[n];
-        int zeroCount = 0;
+        int l = nums.length;
+        int[] arr = new int[l];
+        int s = 0;
+        int e = l - 1;
 
-        // Step 1: Apply operations on the array
-        for (int index = 0; index < n - 1; index++) {
-            if (nums[index] == nums[index + 1] && nums[index] != 0) {
-                nums[index] *= 2;
-                nums[index + 1] = 0;
+        for (int i = 0; i < l; i++) {
+            if (nums[i] == 0) {
+                arr[e] = 0;
+                e--;
+            } 
+            else if (i < l - 1 && nums[i] == nums[i + 1]) { // Avoid out-of-bounds error
+                arr[s] = nums[i] * 2;
+                nums[i + 1] = 0;
+                s++;
+            } 
+            else if (i == l - 1 || nums[i] != nums[i + 1]) { // Ensure last element is processed
+                arr[s] = nums[i];
+                s++;
             }
         }
 
-        // Step 2: Move non-zero elements to the front
-        for (int num : nums) {
-            if (num != 0) {
-                modifiedNums[zeroCount++] = num;
-            }
-        }
-
-        // Step 3: Append zeros to maintain the original size
-        while (zeroCount < n) {
-            modifiedNums[zeroCount++] = 0;
-        }
-
-        return modifiedNums;
+        return arr; // Return statement added
     }
 }
