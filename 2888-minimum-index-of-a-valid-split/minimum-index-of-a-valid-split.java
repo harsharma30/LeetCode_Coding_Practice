75cbf4cbee37620ca337faa876898f29f0f256f6
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2025-03-27 20:08:12 +0530

    Time: 48 ms (23.08%) | Memory: 57.7 MB (52.56%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -5,18 +5,15 @@ class Solution {
         Map<Integer, Integer> secondMap = new HashMap<>();
         int n = nums.size();
 
-        // Add all elements of nums to secondMap
         for (int num : nums) {
             secondMap.put(num, secondMap.getOrDefault(num, 0) + 1);
         }
 
         for (int index = 0; index < n; index++) {
-            // Create split at current index
             int num = nums.get(index);
             secondMap.put(num, secondMap.get(num) - 1);
             firstMap.put(num, firstMap.getOrDefault(num, 0) + 1);
 
-            // Check if valid split
             if (
                 firstMap.get(num) * 2 > index + 1 &&
                 secondMap.get(num) * 2 > n - index - 1
@@ -25,7 +22,6 @@ class Solution {
             }
         }
 
-        // No valid split exists
         return -1;
     }
 }
